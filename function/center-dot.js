0e29d2fba233db11460e385ae4dac0ef30804f84
joaquinAkAjuan <112825010+joaquinAkAjuan@users.noreply.github.com>
2026-05-13 10:18:57 +0200

    center dot zodat je niet hoeft te raden waar je op klikt

---
+++ function/center-dot.js
@@ -0,0 +1,12 @@
+const centerDot = document.createElement('div');
+centerDot.style.position = 'fixed';
+centerDot.style.left = '50%';
+centerDot.style.top = '50%';
+centerDot.style.width = '8px';
+centerDot.style.height = '8px';
+centerDot.style.borderRadius = '999px';
+centerDot.style.transform = 'translate(-50%, -50%)';
+centerDot.style.background = 'rgba(255, 255, 255, 0.95)';
+centerDot.style.pointerEvents = 'none';
+centerDot.style.zIndex = '20';
+document.body.appendChild(centerDot);
